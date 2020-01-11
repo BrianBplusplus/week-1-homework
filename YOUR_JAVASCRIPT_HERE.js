b15e1c563b1e6a6590faa29e927cdc26f19cfda6
BrianBplusplus <brian@nieuwenhuijzen.net>
2020-01-11 16:10:21 +0100

    Added some finishing touches

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,5 +1,5 @@
 const hero = {
-  name: 'Brian',
+  name: '',
   heroic: true,
   inventory: [],
   health: 10, //set back to 10 at end
@@ -12,13 +12,11 @@ const hero = {
 const enemies = {
   enemy1: {
     health: 10,
-    damage: 2,
-    url: '/images/imageEnemy',
+    damage: 3,
   },
   enemy2: {
     health: 20,
     damage: 4,
-    url: '/images/imageEnemy',
   },
 };
 
@@ -36,17 +34,17 @@ function rest(person) {
   };
 };
 
-function pickUpItem(person, weapon){
+function pickUpItem(person, weapon) {
   person.inventory.push(weapon);
 };
 
-function equipWeapon(person){
+function equipWeapon(person) {
   if (person.inventory.length >= 1) {
     person.weapon = person.inventory[0];
   };
 };
 
-function inputName(){
+function inputName() {
   const name = prompt("Enter your name here:");
   const nameElement = document.createElement('h2');
 
@@ -54,26 +52,35 @@ function inputName(){
 
   if (name != false) { 
     const nameLocation = document.getElementById('name');
-    nameLocation.innerHTML = ''; //This line makes the button dissapear
+    nameLocation.innerHTML = '';
     nameLocation.appendChild(nameElement);
+    hero.name = name;
+    displayStats(hero);
    } else { 
     alert('Please input your name');
     return;
   };
 };
 
-function displayStats(person){
+function displayStats(person) {
+  const name = person.name;
   const hearts = person.health;
   const weapon = person.weapon.type;
   const damage = person.weapon.damage;
 
+  const nameElement = document.createElement('p');
   const heartsElement = document.createElement('p');
   const weaponElement = document.createElement('p');
   const damageElement = document.createElement('p');
 
+  nameElement.innerHTML = `Name: ${name}`;
   heartsElement.innerHTML = `Life: ${hearts}`; 
   weaponElement.innerHTML = `Equiped Weapon: ${weapon}`;
   damageElement.innerHTML = `Attack power: ${damage}`;
+
+  const nameLocation = document.getElementById('playername');
+  nameLocation.innerHTML = '';
+  nameLocation.appendChild(nameElement);
 
   const heartsLocation = document.getElementById('hearts');
   heartsLocation.innerHTML = '';
@@ -89,7 +96,6 @@ function displayStats(person){
 
 };
 
-
 //TODO: These functions are not dynamic yet
 function removeImage() {
   document.getElementById('dagger').className = "removed";
@@ -99,7 +105,7 @@ function battleEnemy(person, enemies) {
   person.health = person.health - enemies.enemy1.damage;
   enemies.enemy1.health = enemies.enemy1.health - person.weapon.damage;
 
-  if (enemies.enemy1.health <= 0){
+  if (enemies.enemy1.health <= 0) {
     alert('YOU HAVE DEFEATED THE EVIL VILLAIN! AMAZING!');
     person.health = 10;
     document.getElementById('enemy').className = "removed";
@@ -107,8 +113,9 @@ function battleEnemy(person, enemies) {
 
   if (person.health <= 0) {
     person.health = 0;
-    alert('GAME OVER');
+    if(!alert('GAME OVER')){window.location.reload();}
   };
 };
+
 displayStats(hero);
 
